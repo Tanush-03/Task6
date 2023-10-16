f6afc689544dd9a18bb2fa757893deaa3a62f57b
Tanush Agrawal <115988508+Tanush-03@users.noreply.github.com>
2023-10-16 12:49:32 +0530

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+
+    "name": "Tanush Agrawal",
+    "role": "Full Stack Developer and Data Analyst",
+    "linkedinId":"https://www.linkedin.com/in/tanush-agrawal-23a7591b0/",
+    "roleDescription": "I am curretnly learning MERN Full stack and I am brushing up my skills in Data Analysis domain.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/tanush-agrawal-23a7591b0/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Tanush-03",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Vellore Institute of Technology and pursuing my B.Tech in Computer Science Engineering with Specialization in Bioinformstics. I am currently learning Full Stack Web Development and brushing up my skills in Data Analysis(learning tools like SQL,Excel,Tableau,PowerBi). I am also aiming to delve into world of Ai and Ml in near future.",
+    "address":"India",
+    "website":"tanush.agrawal03@gmail.com",
+    "education":[
+      {
+        "UniversityName":"The Vellore Insitute of Technology",
+        "specialization":"Computer Science with Specialization in Bioinformatics",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2025"
+        
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Centre for Social Entrepreneurship Development",
+        "specialization":"Core committee member in Events,Expansions and Marketing Team",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2023",
+        "Achievements":"Successfully conducted Startup Street 7.0 event in GRAVITAS'22(tech fest of VIT)"
+      },
+    ],
+    // "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Expressjs"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"SQL"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project1",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+        "url":"https://arnav240703.github.io/html_css_0026/"
+      },
+      {
+        "name":"Project2",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project3",
+        "description":"mobileapp",  
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project4",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Your designing skills are truly impressive and bring a unique and creative touch to everything you create.",
+        "name":"Sparsh"
+      },
+      {
+        "description":"Your web development skills are impressive and consistently deliver outstanding results.",
+        "name":"Aditya"
+      }
+    ]
+  }
+  
+  export default resumeData
